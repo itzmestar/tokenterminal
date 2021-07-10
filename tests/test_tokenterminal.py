1c9302df74c63df1428c1a432cf7c216cc1501aa
Tarique Anwer <itzmetariq@gmail.com>
2021-07-10 18:12:28 +0530

    testcases added

--- tests/test_tokenterminal.py
+++ tests/test_tokenterminal.py
@@ -1,15 +1,21 @@
 import pytest
 from tokenterminal import TokenTerminal
+import os
 
 
 @pytest.fixture(scope='module')
-def token_terminal():
-    return TokenTerminal(key='xxxxx-xxxx-xxxx-xxxx-xxxxxxxx')
+def terminal():
+    return TokenTerminal(key=os.getenv('API_KEY'))
 
 
+@pytest.mark.usefixtures('terminal')
 class TestTokenTerminal:
-    def test_get_all_projects(self):
-        assert False
+    def test_get_all_projects(self, terminal):
+        projects = terminal.get_all_projects()
+        assert type(projects) is list
+        assert len(projects) > 0
 
-    def test_get_historical_metrics(self):
-        assert False
+    def test_get_historical_metrics(self, terminal):
+        project_metrics = terminal.get_historical_metrics('0x')
+        assert type(project_metrics) is list
+        assert len(project_metrics) > 0
